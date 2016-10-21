22c78168abeea2aae4132cf4bfd1ccee77acdba9
lucas <currahl@yahoo.ca>
2016-10-21 00:18:01 -0400

    Debugging line added

--- wp-content/plugins/collage-page/collage.php
+++ wp-content/plugins/collage-page/collage.php
@@ -36,6 +36,7 @@ function refresh_cloudinary_urls() {
         $sql = "INSERT INTO $table_name (p_url, p_name) SELECT '$url', '$tmp' WHERE NOT EXISTS (SELECT 1 FROM $table_name WHERE p_name='$tmp');";
         $wpdb->query($sql);
         echo $wpdb->last_query;
+        echo $wpdb->last_error;
     }
     remove_deleted_images();
     delete_duplicate_p_urls();
