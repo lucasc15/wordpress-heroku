71991269898538f7ff366e03dd2c467b0fa0c37d
lucas <currahl@yahoo.ca>
2016-10-22 11:23:01 -0400

    Final edits for appearance of theme

--- wp-content/themes/photogallery/function.php
+++ wp-content/themes/photogallery/function.php
@@ -4,3 +4,8 @@ function enqueue_theme_style() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_style');
+
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
